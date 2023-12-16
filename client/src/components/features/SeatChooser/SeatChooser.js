8994a99bc120aa206064ae9553d7a928825b8239
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-12-16 12:49:29 +0100

    Add free/total seats

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -1,7 +1,8 @@
-import { useEffect, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, Progress, Alert } from 'reactstrap';
-import { getSeats, loadSeatsRequest, getRequests } from '../../../redux/seatsRedux';
+import io from 'socket.io-client';
+import { getSeats, loadSeats, loadSeatsRequest, getRequests } from '../../../redux/seatsRedux';
 
 import './SeatChooser.scss';
 
@@ -9,23 +10,41 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
   const dispatch = useDispatch();
   const seats = useSelector(getSeats);
   const requests = useSelector(getRequests);
-
-  const loadSeats = useCallback(async () => {
-    try {
-      await dispatch(loadSeatsRequest());
-    } catch (error) {
-      console.error('Error loading seats:', error);
-    }
-  }, [dispatch]);
+  const [socket, setSocket] = useState();
+  const [freeSeatsSummary, setFreeSeatsSummary] = useState({ totalSeats: 50 });
 
   useEffect(() => {
-    loadSeats();
-    const intervalId = setInterval(() => {
-      loadSeats();
-    }, 120000);
+    dispatch(loadSeatsRequest());
 
-    return () => clearInterval(intervalId);
-  }, [dispatch, loadSeats]);
+    const newSocket = io(process.env.NODE_ENV === 'production' ? '' : 'ws://localhost:8000', { transports: ['websocket'] });
+    setSocket(newSocket);
+    newSocket.on('seatsUpdated', (updatedSeats) => {
+      dispatch(loadSeats(updatedSeats));
+      const updatedSummary = calculateFreeSeats(updatedSeats, chosenDay);
+      setFreeSeatsSummary(updatedSummary);
+    });
+
+   
+    newSocket.on('seatsSummaryUpdated', (summary) => {
+      setFreeSeatsSummary(summary);
+    });
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, [dispatch, chosenDay]);
+
+  const calculateFreeSeats = (updatedSeats, day) => {
+    const totalSeats = 50;
+    const takenSeats = updatedSeats.filter(seat => seat.client && seat.day === day).length;
+    const freeSeats = totalSeats - takenSeats;
+
+    return {
+      totalSeats: totalSeats,
+      takenSeats: takenSeats,
+      freeSeats: freeSeats,
+    };
+  };
 
   const isTaken = (seatId) => {
     return seats.some(item => item.seat === seatId && item.day === chosenDay);
@@ -37,9 +56,16 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
     else return <Button key={seatId} color="primary" className="seats__seat" outline onClick={(e) => updateSeat(e, seatId)}>{seatId}</Button>;
   };
 
+  
+  useEffect(() => {
+    const initialSummary = calculateFreeSeats(seats, chosenDay);
+    setFreeSeatsSummary(initialSummary);
+  }, [seats, chosenDay]);
+
   return (
     <div>
       <h3>Pick a seat</h3>
+      <p>Free seats: {freeSeatsSummary.freeSeats}/{freeSeatsSummary.totalSeats}</p>
       <div className="mb-4">
         <small id="pickHelp" className="form-text text-muted ms-2"><Button color="secondary" /> – seat is already taken</small>
         <small id="pickHelpTwo" className="form-text text-muted ms-2"><Button outline color="primary" /> – it's empty</small>
