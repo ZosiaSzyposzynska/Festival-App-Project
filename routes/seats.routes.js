d2e07505538916644b74cbced2dcf4f2d1bce61c
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-12-16 12:51:12 +0100

    Add seats routes

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,8 +1,18 @@
-// seats.routes.js
 const express = require('express');
 const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 const db = require('../db');
+
+const getSeatsSummary = (day) => {
+  const totalSeats = 50; // liczba wszystkich miejsc
+  const takenSeats = db.seats.filter(item => item.day === day && item.client).length; 
+
+  return {
+    totalSeats: totalSeats,
+    takenSeats: takenSeats,
+    freeSeats: totalSeats - takenSeats,
+  };
+};
 
 router.get('/', (req, res) => {
   res.json(db.seats);
@@ -18,25 +28,28 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   const { day, seat, client, email } = req.body;
 
-  if (day && seat && client && email) {
-    // Sprawdź, czy miejsce jest już zajęte na wybrany dzień
+  try {
     const isSeatTaken = db.seats.some(item => item.seat === seat && item.day === day);
 
     if (isSeatTaken) {
-      // Miejsce jest już zajęte, zwróć odpowiedni komunikat i status HTTP 409 Conflict
       res.status(409).json({ message: 'The slot is already taken...' });
     } else {
-      // Miejsce jest dostępne, zarezerwuj je
       const newSeat = { id: uuidv4(), day, seat, client, email };
       db.seats.push(newSeat);
+
+      req.io.emit('seatsUpdated', db.seats);
+
+      const updatedSummary = getSeatsSummary(day);
+      req.io.emit('seatsSummaryUpdated', updatedSummary);
+
       res.json({ message: 'OK' });
     }
-  } else {
-    // Niepoprawne dane w żądaniu, zwróć odpowiedni komunikat i status HTTP 400 Bad Request
-    res.status(400).json({ message: 'Bad request. Missing required fields in request body.' });
+  } catch (error) {
+ 
+    next(error);
   }
 });
 
@@ -46,15 +59,16 @@ router.put('/:id', (req, res) => {
   if (seat) {
     const { day, seat: newSeat, client, email } = req.body;
 
-    if (day && newSeat && client && email) {
-      seat.day = day;
-      seat.seat = newSeat;
-      seat.client = client;
-      seat.email = email;
-      res.json({ message: 'OK' });
-    } else {
-      res.status(400).json({ message: 'Bad request. Missing required fields in request body.' });
-    }
+    seat.day = day;
+    seat.seat = newSeat;
+    seat.client = client;
+    seat.email = email;
+    req.io.emit('seatsUpdated', db.seats);
+
+    const updatedSummary = getSeatsSummary(day);
+    req.io.emit('seatsSummaryUpdated', updatedSummary);
+
+    res.json({ message: 'OK' });
   } else {
     res.status(404).json({ message: 'Not found' });
   }
@@ -64,7 +78,13 @@ router.delete('/:id', (req, res) => {
   const index = db.seats.findIndex(item => item.id === parseInt(req.params.id));
 
   if (index !== -1) {
+    const deletedSeat = db.seats[index];
     db.seats.splice(index, 1);
+    req.io.emit('seatsUpdated', db.seats);
+
+    const updatedSummary = getSeatsSummary(deletedSeat.day);
+    req.io.emit('seatsSummaryUpdated', updatedSummary);
+
     res.json({ message: 'OK' });
   } else {
     res.status(404).json({ message: 'Not found' });
