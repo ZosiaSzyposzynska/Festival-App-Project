04c30b8bf0c771698d292bb3e6e846bf2c957658
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-12-16 08:35:30 +0100

    Add seats routes

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,8 +1,12 @@
+// seats.routes.js
 const express = require('express');
 const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 const db = require('../db');
 
+router.get('/', (req, res) => {
+  res.json(db.seats);
+});
 
 router.get('/:id', (req, res) => {
   const seat = db.seats.find(item => item.id === parseInt(req.params.id));
@@ -18,10 +22,20 @@ router.post('/', (req, res) => {
   const { day, seat, client, email } = req.body;
 
   if (day && seat && client && email) {
-    const newSeat = { id: uuidv4(), day, seat, client, email };
-    db.seats.push(newSeat);
-    res.json({ message: 'OK' });
+    // Sprawdź, czy miejsce jest już zajęte na wybrany dzień
+    const isSeatTaken = db.seats.some(item => item.seat === seat && item.day === day);
+
+    if (isSeatTaken) {
+      // Miejsce jest już zajęte, zwróć odpowiedni komunikat i status HTTP 409 Conflict
+      res.status(409).json({ message: 'The slot is already taken...' });
+    } else {
+      // Miejsce jest dostępne, zarezerwuj je
+      const newSeat = { id: uuidv4(), day, seat, client, email };
+      db.seats.push(newSeat);
+      res.json({ message: 'OK' });
+    }
   } else {
+    // Niepoprawne dane w żądaniu, zwróć odpowiedni komunikat i status HTTP 400 Bad Request
     res.status(400).json({ message: 'Bad request. Missing required fields in request body.' });
   }
 });
@@ -30,11 +44,11 @@ router.put('/:id', (req, res) => {
   const seat = db.seats.find(item => item.id === parseInt(req.params.id));
 
   if (seat) {
-    const { day, seat, client, email } = req.body;
+    const { day, seat: newSeat, client, email } = req.body;
 
-    if (day && seat && client && email) {
+    if (day && newSeat && client && email) {
       seat.day = day;
-      seat.seat = seat;
+      seat.seat = newSeat;
       seat.client = client;
       seat.email = email;
       res.json({ message: 'OK' });
